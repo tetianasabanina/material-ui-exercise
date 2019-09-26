5fec251cd1e31e88e7f7577935b18fe2095ac364
Sabanina Tetiana <s1900565@edu.bc.fi>
2019-09-26 12:53:13 +0300

    Add: navbar and postlist issue: grid doesn't work.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
 
-const App = () => {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+import NavBar from './Components/NavBar';
+import PostList from './Components/PostList';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <AppBar color="primary" position="static">
+        <Toolbar>
+            <Typography variant="h5"
+              color="inherit">
+              Material UI
+           </Typography>
+           <NavBar />
+          </Toolbar>
+        </AppBar>
+        <PostList />
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/Content/posts.js
@@ -0,0 +1,21 @@
+const posts = [
+    {
+     title: "Post One",
+     excerpt: "Here is will be the card's excerpt...",
+     image: "./img/porcelandia1.jpg"
+    },
+   
+    {
+     title: "Post Two",
+     excerpt: "Here is will be the card's excerpt...",
+     image: "./img/porcelandia1.jpg"
+    },
+   
+    {
+     title: "Post Three",
+     excerpt: "Here is will be the card's excerpt...",
+     image: "./img/porcelandia1.jpg"
+    }
+   ]
+
+   export {posts};
